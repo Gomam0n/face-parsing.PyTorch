818ec3d938054144f08492712ad8b1cedab9d6e9
Yifan Qian <39645704+Gomam0n@users.noreply.github.com>
2021-11-05 13:01:24 -0500

    Update prepropess_data.py

--- prepropess_data.py
+++ prepropess_data.py
@@ -7,9 +7,9 @@ import cv2
 from transform import *
 from PIL import Image
 
-face_data = '/home/zll/data/CelebAMask-HQ/CelebA-HQ-img'
-face_sep_mask = '/home/zll/data/CelebAMask-HQ/CelebAMask-HQ-mask-anno'
-mask_path = '/home/zll/data/CelebAMask-HQ/mask'
+face_data = '/root/ECE549/CelebAMask-HQ/CelebA-HQ-img'
+face_sep_mask = '/root/ECE549/CelebAMask-HQ/CelebAMask-HQ-mask-anno'
+mask_path = '/root/ECE549/CelebAMask-HQ/mask'
 counter = 0
 total = 0
 for i in range(15):
